df7be34606356a99da10937be3d28f0099acd801
Keeyon Ebrahimi <keeyon2@gmail.com>
2012-04-06 13:37:31 -0600

    Networking Done, I think, I hope.  I took care of the max players in game.  Got away from the network Instantiate and went to RPC. Should be much better.

--- Assets/Omniscient Assets/HUD/HUDSampleControllerNetworking.cs
+++ Assets/Omniscient Assets/HUD/HUDSampleControllerNetworking.cs
@@ -6,6 +6,7 @@ public class HUDSampleControllerNetworking : MonoBehaviour, IHUDSearchingViewCon
 {
 	private HUDSearchingView _searchingView;
 	private HUDGameView _gameView;
+	private bool hasStarted = false;
 	// Use this for initialization
 	void Start () 
 	{
@@ -18,9 +19,21 @@ public class HUDSampleControllerNetworking : MonoBehaviour, IHUDSearchingViewCon
 	// Update is called once per frame
 	void Update () 
 	{
+		if ((hasStarted == false) && (Network.peerType == NetworkPeerType.Server))
+		{
+			if (Network.maxConnections == Network.connections.Length + 1)
+			{
+				networkView.RPC("GameIsFull",RPCMode.All);
+			}
+		}
 		
 	}
 	
+	[RPC]
+	public void GameIsFull()
+	{
+		hasStarted = true;
+	}
 	// IHUDSearchingViewController methods
 	public void HUDSearchingViewPrintButtonPressed()
 	{
@@ -56,15 +69,35 @@ public class HUDSampleControllerNetworking : MonoBehaviour, IHUDSearchingViewCon
 	
 	public void HUDGameViewFireButtonPressed()
 	{
-		_gameView.Energy = _gameView.Energy - 1.0f;
-		GameObject cam = GameObject.Find("ARCamera");
-		Debug.Log(cam.transform.position);
+		Debug.Log ("Max Connections = " + Network.maxConnections);
+		Debug.Log ("Network Connections = " + Network.connections.Length);
+		Debug.Log ("Network Upper Bound = " + Network.connections.GetUpperBound(0));
+		
+		if (hasStarted)
+		{
+			networkView.RPC("ShootWithoutNetworkInstantiate",RPCMode.All);
+			_gameView.Energy = _gameView.Energy - 1.0f;
+		}
+
+		
+		//networkView.RPC("ShootWithoutNetworkInstantiate",RPCMode.All);
+		
+		/*
 		GameObject thePrefab = (GameObject)Resources.Load("StrongBall");
 		GameObject instance = (GameObject)Network.Instantiate(thePrefab, cam.transform.position, cam.transform.rotation, 0);
 		Vector3 fwd = cam.transform.forward * 50000;
 		instance.rigidbody.AddForce(fwd);
-		
-		
+		*/
+	}
+	
+	[RPC]
+	public void ShootWithoutNetworkInstantiate()
+	{
+		GameObject cam = GameObject.Find("ARCamera");
+		GameObject thePrefab = (GameObject)Resources.Load("StrongBall");
+		GameObject instance = (GameObject)Instantiate(thePrefab, cam.transform.position, cam.transform.rotation);
+		Vector3 fwd = cam.transform.forward * 50000;
+		instance.rigidbody.AddForce(fwd);
 	}
 	
 	public void HUDGameViewPauseButtonPressed()
--- Assets/Omniscient Assets/Networking/InstantiatingTemple.cs
+++ Assets/Omniscient Assets/Networking/InstantiatingTemple.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 
 public class InstantiatingTemple : MonoBehaviour {
 	public GameObject temple;
+	public bool hasCreated = false;
+
 
 	// Use this for initialization
 	void Start () {
-		
-		if (Network.peerType == NetworkPeerType.Server)
+		//networkView.RPC("SetUpTemple", RPCMode.All);
+		Debug.Log("Max Connections: " + Network.maxConnections);
+		Debug.Log("Current Players: " + Network.connections.Length);
+	/*	if (Network.peerType == NetworkPeerType.Server)
 		{
 			GameObject templeObject;
 			Transform templeTransform;
@@ -18,26 +22,72 @@ public class InstantiatingTemple : MonoBehaviour {
 			templeY = transform.position.y + 17.0f;
 			templeZ = transform.position.z;
 		
+			NetworkViewID viewID = Network.AllocateViewID();
 		
 			//Network.Instantiate(temple, transform.position, transform.rotation,0);
 			Network.Instantiate(temple, new Vector3(templeX, templeY, templeZ), transform.rotation, 0);
 			//Instantiate(temple, transform.position, transform.rotation);
 		
-			//Find Object
-			templeObject = GameObject.Find("Temple1(Clone)");
-		
-			//Give Network View
-			templeObject.AddComponent("NetworkView");
-		
-			//Grab transform to make it child of ImageTarget
-			//templeTransform = templeObject.transform;
-			//templeTransform.parent = transform;
-			templeObject.transform.parent = transform;
-		}
+			networkView.RPC("SetUpTemple", RPCMode.All, viewID);
+
+		} */
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if ((hasCreated == false) && (Network.peerType == NetworkPeerType.Server))
+		{
+			if (Network.maxConnections == Network.connections.Length + 1)
+			{
+				Debug.Log("Temple Being Created");
+				networkView.RPC("SetUpTemple", RPCMode.All);
+				
+			}
+			
+		}
+		
+	}
+	
+	void OnGUI(){
+		if (hasCreated == false)
+		{
+			GUI.Label(new Rect(10, 10, 150, 150), "Waiting for All Players to Join Game");
+		}
+	}
+	
+	[RPC]
+	void SetUpTemple()
+	{
+		GameObject templeObject;
+		
+		Transform templeTransform;
+		float templeX, templeY, templeZ;
+		
+		templeX = transform.position.x;
+		templeY = transform.position.y + 17.0f;
+		templeZ = transform.position.z;
+		
+		
+		//Network.Instantiate(temple, transform.position, transform.rotation,0);
+		Instantiate(temple, new Vector3(templeX, templeY, templeZ), transform.rotation);
+			
+		
+		//Find Object
+		//templeObject = GameObject.Find("Temple1(Clone)");
+		
+		//Give Network View
+		//templeObject.AddComponent("NetworkView");
+		
+		//Grab transform to make it child of ImageTarget
+		//templeObject.transform.parent = transform;
+		
+		//templeTransform = templeObject.transform;
+		//templeTransform.parent = transform;
+		//templeObject.transform.localScale += new Vector3(2, 2, 2);
+		
+		hasCreated = true;
+				
+		
 	}
 }
